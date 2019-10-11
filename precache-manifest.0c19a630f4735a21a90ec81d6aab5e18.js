0614fc84d2df0e756484e9d4fde12040c4220745
CKanishka <kanishka.c@probyto.com>
2019-10-12 00:18:39 +0530

    Updates

---
+++ precache-manifest.0c19a630f4735a21a90ec81d6aab5e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccf021b045861b6d5c222c8224dd2a6",
+    "url": "/Products-Page/index.html"
+  },
+  {
+    "revision": "ef8f4c7e378ae8683f1c",
+    "url": "/Products-Page/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "76488c9da16aba2a317c",
+    "url": "/Products-Page/static/css/main.2b30e3a1.chunk.css"
+  },
+  {
+    "revision": "ef8f4c7e378ae8683f1c",
+    "url": "/Products-Page/static/js/2.42a40c4a.chunk.js"
+  },
+  {
+    "revision": "76488c9da16aba2a317c",
+    "url": "/Products-Page/static/js/main.d702873e.chunk.js"
+  },
+  {
+    "revision": "5cf7433b73213706cabe",
+    "url": "/Products-Page/static/js/runtime-main.084a9f92.js"
+  }
+]);
